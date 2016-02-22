e748913f82a66cb57a10301af308065eed3a4635
Foxboron <morten@linderud.pw>
2016-02-22 23:54:04 +0100

    Init

---
+++ src/no/uib/info216/Main.java
@@ -0,0 +1,10 @@
+package no.uib.info216;
+
+/**
+ * Created by fox on 2/22/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
