8f13a22821d1b84db0e30873f49f78cef37793ae
Eirik Sjøvoll <eirik.sjovoll@student.uib.no>
2016-02-23 16:15:24 +0100

    weather api .xml parse to rdf turtle .ttl
    added two classes, jenaTest for importing .xml doc and creating hmaps and arrays from, RDFParser for taking the arrays and building an rdf doc with turtle syntax.

--- src/no/uib/info216/Main.java
+++ src/no/uib/info216/Main.java
@@ -4,7 +4,10 @@ package no.uib.info216;
  * Created by fox on 2/22/16.
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        RDFparser rp = new RDFparser();
+
     }
 }
