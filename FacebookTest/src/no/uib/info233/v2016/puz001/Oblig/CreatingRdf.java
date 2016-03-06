247e4e54a70dac8c3ad12acea96a321c34dfb05b
Lillevik <mariusfalch@hotmail.com>
2016-03-06 02:06:42 +0100

    writeToFile() with turtle syntax
    Added a method which now creates a file containing all the interests of
    the current user as rdt turtle syntax.

--- FacebookTest/src/no/uib/info233/v2016/puz001/Oblig/CreatingRdf.java
+++ FacebookTest/src/no/uib/info233/v2016/puz001/Oblig/CreatingRdf.java
@@ -3,61 +3,301 @@
  */
 package no.uib.info233.v2016.puz001.Oblig;
 
-import java.io.ObjectOutputStream;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.system.StreamRDFWriter;
 
 /**
  * @author mariuslillevik
  *
- *This class will be creating an RDF file that represents the
- *users and their interests. Nothing yet.
+ *         This class will be creating an RDF file that represents the users and
+ *         their interests. Nothing yet.
  */
 public class CreatingRdf {
 
-//	private File file = new File("Facebook.rdf");
-//	private StreamRDFWriter writer = new StreamRDFWriter();
-	private ObjectOutputStream os;
-	public void createModel(){
-		
-	Model model = ModelFactory.createDefaultModel();
-	Resource res = model.createResource("http://info216.uib.no/resource/hellojuna");	
-		//Predicate
-			
-	Property prop = model.createProperty("http://info216.uib.no/resource/hellojuna");
+	private Model model = ModelFactory.createDefaultModel();
+	private Interests ir = new Interests();
+	private Resource person = model.createProperty("http://info216.uib.no/resource/hellojuna");
+	private Resource res = model.createResource("http://info216.uib.no/resource/hellojuna");
 
-			
-	res.addLiteral(prop, "Hello world");
-			
-	model.write(System.out, "TURTLE");
-	
-	
-	Property tvShow = model.createProperty("http://info216.uib.no/resource/hellojuna");
-	Property movie = model.createProperty("http://info216.uib.no/resource/hellojuna");
-	Property music = model.createProperty("http://info216.uib.no/resource/hellojuna");
-	Property book = model.createProperty("http://info216.uib.no/resource/hellojuna");
-	Property games = model.createProperty("http://info216.uib.no/resource/hellojuna");
-	Property events = model.createProperty("http://info216.uib.no/resource/hellojuna");
+	private Property tvShow = model.createProperty("http://info216.uib.no/resource/tvShow");
+	private Property movie = model.createProperty("http://info216.uib.no/resource/movie");
+	private Property music = model.createProperty("http://info216.uib.no/resource/music");
+	private Property book = model.createProperty("http://info216.uib.no/resource/book");
+	private Property games = model.createProperty("http://info216.uib.no/resource/game");
+	private Property events = model.createProperty("http://info216.uib.no/resource/event");
+	private Property likes = model.createProperty("http://info216.uib.no/resource/likes");
 
+	/**
+	 * This method creates the jena model by adding all the interests to the
+	 * model using the method created below.
+	 */
+	public void createModel() {
+		addTvShows();
+		addMovies();
+		addMusic();
+		addGames();
+		addBooks();
+		addEvent();
+		addLike();
 
+	}
 
-			Resource person = model.createProperty("http://info216.uib.no/resource/hellojuna");
-			person.addLiteral(tvShow, "1");
-			person.addLiteral(movie, "1");
-			person.addLiteral(music, "1");
-			person.addLiteral(book, "1");
-			person.addLiteral(games, "1");
-			person.addLiteral(events, "1");
-			
-			System.out.println(person);
-			
-			
-			StreamRDFWriter.write(os, model.getGraph(), Lang.TURTLE) ;
+	/**
+	 * This is a method which adds all the games of the current user to the jena
+	 * model and adds a uri to it.
+	 */
+	public void addGames() {
+		for (int i = 0; i < ir.getGames().size(); i++) {
 
+			String gameName = ir.getGames().get(i);
+			String name = ir.getGames().get(i);
+
+			@SuppressWarnings("unused")
+			Resource game = this.model.createResource(gameName).addProperty(games, name);
+		}
+	}
+
+	/**
+	 * This is a method which adds all the movies of the current user to the
+	 * jena model and adds a uri to it.
+	 */
+	public void addMovies() {
+		for (int i = 0; i < ir.getMovies().size(); i++) {
+
+			String gameName = ir.getMovies().get(i);
+			String name = ir.getMovies().get(i);
+
+			@SuppressWarnings("unused")
+			Resource movies = this.model.createResource(gameName).addProperty(movie, name);
+		}
+	}
+
+	/**
+	 * This is a method which adds all the tvShows of the current user to the
+	 * jena model and adds a uri to it.
+	 */
+	public void addTvShows() {
+		for (int i = 0; i < ir.getTvShows().size(); i++) {
+
+			String gameName = ir.getTvShows().get(i);
+			String name = ir.getTvShows().get(i);
+
+			@SuppressWarnings("unused")
+			Resource movies = this.model.createResource(gameName).addProperty(tvShow, name);
+		}
+	}
+
+	/**
+	 * This is a method which adds all the music of the current user to the jena
+	 * model and adds a uri to it.
+	 */
+	public void addMusic() {
+		for (int i = 0; i < ir.getMusic().size(); i++) {
+
+			String gameName = ir.getMusic().get(i);
+			String name = ir.getMusic().get(i);
+
+			@SuppressWarnings("unused")
+			Resource musics = this.model.createResource(gameName).addProperty(music, name);
+		}
+	}
+
+	/**
+	 * This is a method which adds all the books of the current user to the jena
+	 * model and adds a uri to it.
+	 */
+	public void addBooks() {
+		for (int i = 0; i < ir.getBook().size(); i++) {
+
+			String gameName = ir.getBook().get(i);
+			String name = ir.getBook().get(i);
+
+			@SuppressWarnings("unused")
+			Resource books = this.model.createResource(gameName).addProperty(book, name);
+		}
+	}
+
+	/**
+	 * This is a method which adds all the events of the current user to the
+	 * jena model and adds a uri to it.
+	 */
+	public void addEvent() {
+		for (int i = 0; i < ir.getEvents().size(); i++) {
+
+			String gameName = ir.getEvents().get(i);
+			String name = ir.getEvents().get(i);
+
+			@SuppressWarnings("unused")
+			Resource movies = this.model.createResource(gameName).addProperty(events, name);
+		}
+	}
+
+	/**
+	 * This is a method which adds all the likes of the current user to the jena
+	 * model and adds a uri to it.
+	 */
+	public void addLike() {
+		for (int i = 0; i < ir.getLikes().size(); i++) {
+
+			String gameName = ir.getLikes().get(i);
+			String name = ir.getLikes().get(i);
+
+			@SuppressWarnings("unused")
+			Resource likesPages = this.model.createResource(gameName).addProperty(likes, name);
+		}
+
+	}
+
+	/**
+	 * @return the model
+	 */
+	public Model getModel() {
+		return model;
+	}
+
+	/**
+	 * @param model
+	 *            the model to set
+	 */
+	public void setModel(Model model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return the person
+	 */
+	public Resource getPerson() {
+		return person;
+	}
+
+	/**
+	 * @param person
+	 *            the person to set
+	 */
+	public void setPerson(Resource person) {
+		this.person = person;
+	}
+
+	/**
+	 * @return the tvShow
+	 */
+	public Property getTvShow() {
+		return tvShow;
+	}
+
+	/**
+	 * @param tvShow
+	 *            the tvShow to set
+	 */
+	public void setTvShow(Property tvShow) {
+		this.tvShow = tvShow;
+	}
+
+	/**
+	 * @return the movie
+	 */
+	public Property getMovie() {
+		return movie;
+	}
+
+	/**
+	 * @param movie
+	 *            the movie to set
+	 */
+	public void setMovie(Property movie) {
+		this.movie = movie;
+	}
+
+	/**
+	 * @return the music
+	 */
+	public Property getMusic() {
+		return music;
+	}
+
+	/**
+	 * @param music
+	 *            the music to set
+	 */
+	public void setMusic(Property music) {
+		this.music = music;
+	}
+
+	/**
+	 * @return the book
+	 */
+	public Property getBook() {
+		return book;
+	}
+
+	/**
+	 * @param book
+	 *            the book to set
+	 */
+	public void setBook(Property book) {
+		this.book = book;
+	}
+
+	/**
+	 * @return the games
+	 */
+	public Property getGames() {
+		return games;
+	}
+
+	/**
+	 * @param games
+	 *            the games to set
+	 */
+	public void setGames(Property games) {
+		this.games = games;
+	}
+
+	/**
+	 * @return the events
+	 */
+	public Property getEvents() {
+		return events;
+	}
+
+	/**
+	 * @param events
+	 *            the events to set
+	 */
+	public void setEvents(Property events) {
+		this.events = events;
+	}
+
+	/**
+	 * @return the res
+	 */
+	public Resource getRes() {
+		return res;
+	}
+
+	/**
+	 * @param res
+	 *            the res to set
+	 */
+	public void setRes(Resource res) {
+		this.res = res;
+	}
+
+	/**
+	 * @return the ir
+	 */
+	public Interests getIr() {
+		return ir;
+	}
+
+	/**
+	 * @param ir
+	 *            the ir to set
+	 */
+	public void setIr(Interests ir) {
+		this.ir = ir;
 	}
 
 }
--- FacebookTest/src/no/uib/info233/v2016/puz001/Oblig/Interests.java
+++ FacebookTest/src/no/uib/info233/v2016/puz001/Oblig/Interests.java
@@ -5,26 +5,35 @@ package no.uib.info233.v2016.puz001.Oblig;
 
 import java.util.ArrayList;
 
+import facebook4j.Book;
+import facebook4j.Event;
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.Game;
+import facebook4j.Like;
+import facebook4j.Movie;
+import facebook4j.Music;
+import facebook4j.Television;
+
 /**
  * @author mariuslillevik
  *
- *The intention for this class is to create some sort of 
- *structure for the different interests for the different users.
- *
+ *         The intention for this class is to create some sort of structure for
+ *         the different interests for the different users.
  */
 public class Interests {
-	
-	private ArrayList<String> tvShows;
-	private String movies;
-	private String music;
-	private String book;
-	private String games;
-	private String events;
-	private ArrayList<Interests> interests = new ArrayList<Interests>();
-	
-	
+
+	private ArrayList<String> tvShows = new ArrayList<String>();
+	private ArrayList<String> movies = new ArrayList<String>();
+	private ArrayList<String> music = new ArrayList<String>();
+	private ArrayList<String> book = new ArrayList<String>();
+	private ArrayList<String> games = new ArrayList<String>();
+	private ArrayList<String> events = new ArrayList<String>();
+	private ArrayList<String> likes = new ArrayList<String>();
+
 	/**
-	 * Constructor for the 
+	 * Constructor for the
+	 * 
 	 * @param shows
 	 * @param movies
 	 * @param music
@@ -32,10 +41,48 @@ public class Interests {
 	 * @param games
 	 * @param events
 	 */
-	public Interests(){//String shows, String movies, String music, String book, String games, String events
+	public Interests() {
 
 	}
 
+	/**
+	 * This is a simple method which prints all the interests the current user
+	 * has and seperates them with categories.
+	 * 
+	 * @param fb
+	 * @throws FacebookException
+	 */
+	public void getInterests(Facebook fb) throws FacebookException {
+
+		for (Television t : fb.getTelevision()) {
+			getTvShows().add(t.getName());
+		}
+
+		for (Movie m : fb.getMovies()) {
+			getMovies().add(m.getName());
+		}
+
+		for (Music m : fb.getMusic()) {
+			getMusic().add(m.getName());
+		}
+
+		for (Book b : fb.getBooks()) {
+			getBook().add(b.getName());
+		}
+
+		for (Game g : fb.getGames()) {
+			getGames().add(g.getName());
+		}
+
+		for (Event e : fb.getEvents()) {
+			getEvents().add(e.getName());
+
+			for (Like l : fb.getUserLikes()) {
+				getLikes().add(l.getName());
+			}
+
+		}
+	}
 
 	/**
 	 * @return the tvShows
@@ -44,113 +91,102 @@ public class Interests {
 		return tvShows;
 	}
 
-
 	/**
-	 * @param tvShows the tvShows to set
+	 * @param tvShows
+	 *            the tvShows to set
 	 */
 	public void setTvShows(ArrayList<String> tvShows) {
 		this.tvShows = tvShows;
 	}
 
-
 	/**
 	 * @return the movies
 	 */
-	public String getMovies() {
+	public ArrayList<String> getMovies() {
 		return movies;
 	}
 
-
 	/**
-	 * @param movies the movies to set
+	 * @param movies
+	 *            the movies to set
 	 */
-	public void setMovies(String movies) {
+	public void setMovies(ArrayList<String> movies) {
 		this.movies = movies;
 	}
-
 
 	/**
 	 * @return the music
 	 */
-	public String getMusic() {
+	public ArrayList<String> getMusic() {
 		return music;
 	}
 
-
 	/**
-	 * @param music the music to set
+	 * @param music
+	 *            the music to set
 	 */
-	public void setMusic(String music) {
+	public void setMusic(ArrayList<String> music) {
 		this.music = music;
 	}
-
 
 	/**
 	 * @return the book
 	 */
-	public String getBook() {
+	public ArrayList<String> getBook() {
 		return book;
 	}
 
-
 	/**
-	 * @param book the book to set
+	 * @param book
+	 *            the book to set
 	 */
-	public void setBook(String book) {
+	public void setBook(ArrayList<String> book) {
 		this.book = book;
 	}
-
 
 	/**
 	 * @return the games
 	 */
-	public String getGames() {
+	public ArrayList<String> getGames() {
 		return games;
 	}
 
-
 	/**
-	 * @param games the games to set
+	 * @param games
+	 *            the games to set
 	 */
-	public void setGames(String games) {
+	public void setGames(ArrayList<String> games) {
 		this.games = games;
 	}
-
 
 	/**
 	 * @return the events
 	 */
-	public String getEvents() {
+	public ArrayList<String> getEvents() {
 		return events;
 	}
 
-
 	/**
-	 * @param events the events to set
+	 * @param events
+	 *            the events to set
 	 */
-	public void setEvents(String events) {
+	public void setEvents(ArrayList<String> events) {
 		this.events = events;
 	}
 
-
 	/**
-	 * @return the interests
+	 * @return the likes
 	 */
-	public ArrayList<Interests> getInterests() {
-		return interests;
+	public ArrayList<String> getLikes() {
+		return likes;
 	}
 
-
 	/**
-	 * @param interests the interests to set
+	 * @param likes
+	 *            the likes to set
 	 */
-	public void setInterests(ArrayList<Interests> interests) {
-		this.interests = interests;
+	public void setLikes(ArrayList<String> likes) {
+		this.likes = likes;
 	}
-	
-	
-	
-	
-	
 
 }
