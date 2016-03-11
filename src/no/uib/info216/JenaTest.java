febc7d7c3791918d00afc8c70c0a0fb5d7c740b5
Eirik Sjøvoll <eirik.sjovoll@student.uib.no>
2016-03-11 16:31:36 +0100

    MORE LISTS111

--- src/no/uib/info216/JenaTest.java
+++ src/no/uib/info216/JenaTest.java
@@ -1,6 +1,7 @@
 package no.uib.info216;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -20,6 +21,12 @@ public class JenaTest {
     private ArrayList<String> nametag = new ArrayList<String>();
     private ArrayList<String> fromtag = new ArrayList<String>();
     private ArrayList<String> totag = new ArrayList<String>();
+    private ArrayList<String> windDirection = new ArrayList<String>();
+    private ArrayList<String> windDirectionName = new ArrayList<>();
+    private ArrayList<String> windSpeed = new ArrayList<String>();
+    private ArrayList<String> windSpeedName = new ArrayList<String>();
+    private ArrayList<String> temprature = new ArrayList<String>();
+
     public ArrayList<HashMap<String,String>> dataStruct = new ArrayList<>();
 
     public void makeSymbolList(){
@@ -60,13 +67,32 @@ public class JenaTest {
             Node symbolNode = eElement.getElementsByTagName("symbol").item(0);
             Element symbolElement = (Element)  symbolNode;
 
+            Node windDirectionNode = eElement.getElementsByTagName("windDirection").item(1);
+            Element directionElement = (Element)  windDirectionNode;
+
+            Node windSpeedNode = eElement.getElementsByTagName("windSpeed").item(2);
+            Element windSpeedElement = (Element)  windSpeedNode;
+
+            Node tempNode = eElement.getElementsByTagName("temperature").item(3);
+            Element tempElement = (Element)  tempNode;
+
             obj.put("name", symbolElement.getAttribute("name"));
             obj.put("from", eElement.getAttribute("from"));
             obj.put("to", eElement.getAttribute("to"));
 
             nametag.add(symbolElement.getAttribute("name"));
             fromtag.add(eElement.getAttribute("from"));
+            windDirection.add(directionElement.getAttribute("deg"));
+            windDirectionName.add(directionElement.getAttribute("name"));
+            windSpeed.add(windSpeedElement.getAttribute("mps"));
+            windSpeedName.add(windSpeedElement.getAttribute("name"));
+            temprature.add(tempElement.getAttribute("value"));
 
+            System.out.println("WIND DIR: " + windDirection);
+            System.out.println("WIND DIRNAME: " + windDirectionName);
+            System.out.println("WIND SPEED: " + windSpeed);
+            System.out.println("WIND SPEEDNAME: " + windSpeedName);
+            System.out.println("TEMP TEMP: " + temprature);
             this.dataStruct.add(obj);
 
         }
